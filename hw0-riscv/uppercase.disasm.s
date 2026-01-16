55303925277aa969cf6bcc1c9da2f9edb1b7fcb1
tiffanylian <tiffanylian04@gmail.com>
2026-01-16 20:53:58 +0000

    rebuilt

--- hw0-riscv/uppercase.disasm.s
+++ hw0-riscv/uppercase.disasm.s
@@ -8,7 +8,7 @@ Disassembly of section .text:
    10074:	ffff2517          	auipc	a0,0xffff2
    10078:	f8c50513          	addi	a0,a0,-116 # 2000 <__DATA_BEGIN__>
 
-0001007c <loop>:
+0001007c <while_loop>:
    1007c:	00050583          	lb	a1,0(a0)
    10080:	02058263          	beqz	a1,100a4 <end_program>
    10084:	f9f58613          	addi	a2,a1,-97
@@ -20,7 +20,7 @@ Disassembly of section .text:
 
 0001009c <next>:
    1009c:	00150513          	addi	a0,a0,1
-   100a0:	fddff06f          	j	1007c <loop>
+   100a0:	fddff06f          	j	1007c <while_loop>
 
 000100a4 <end_program>:
    100a4:	0000006f          	j	100a4 <end_program>
